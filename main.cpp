b4b43b9512c9554fdc30c1a74d1a7364a54dca3c
Babich <ivan20.babich@gmail.com>
2025-09-20 02:47:01 +0300

    minor update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+bool isTheGameOver() {
+
+}
+
+void bot(char field, int playerRow, int playerCol) {
+    
+}
+
 int main() {
     while (true) {
         char field[3][3];
@@ -11,16 +19,19 @@ int main() {
             std::cout << "Please, choose currectly" << std::endl;
             continue;
         }
-
-        int playerCol, playerRow;
-        for (int i = 0; i < 3; i++) {
-            std::cout << "|";
-            for (int j = 0; j < 3; j++) {
-                std::cout << field[i][j] << "|";
+        while (!isTheGameOver) {
+            int playerCol, playerRow;
+            std::cout << "  1 2 3\n";
+            for (int i = 0; i < 3; i++) {
+                std::cout << i+ 1 << " |";
+                for (int j = 0; j < 3; j++) {
+                    std::cout << field[i][j] << "|";
+                }
+                std::cout << "\n";
             }
-            std::cout << "\n";
+            std::cout << "Choose the column and the row:" << std::endl;
+            std::cin >> playerCol >> playerRow;
+            field[playerCol + 1][playerRow + 1] = player;
         }
-        std::cout << "Choose the column and the row:" << std::endl;
-        std::cin >> playerCol >> playerRow;
     }
 }
