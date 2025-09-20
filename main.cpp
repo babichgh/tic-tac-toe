99d8fff553e2541a636bebc5835ce1380191625a
Babich <ivan20.babich@gmail.com>
2025-09-21 02:43:13 +0300

    basics done

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ bool isTheGameOver(char field[3][3]) {
         (field[0][0] == 'o' && field[1][1] == 'o' && field[2][2] == 'o') ||
         (field[0][2] == 'o' && field[1][1] == 'o' && field[2][0] == 'o')
         )) {
-        std::cout << "o win!" << std::endl;
+        std::cout << "o win!\n" << std::endl;
         return true;
     } else {
         return false;
@@ -39,29 +39,33 @@ bool isTheGameOver(char field[3][3]) {
 }
 
 int main() {
+    char field[3][3];
+    char player = 'x';
     while (true) {
-        char field[3][3];
-
-        char player;
-        std::cout << "Choose:\nx - cross; o - zero" << std::endl;
-        std::cin >> player;
-        if (player != 'x' || player != 'o') {
-            std::cout << "Please, choose currectly" << std::endl;
-            continue;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = ' ';
+            }
         }
-        while (!isTheGameOver) {
-            int playerCol, playerRow;
-            std::cout << "  1 2 3\n";
+        while (!isTheGameOver(field)) {
+            int playerRow, playerCol;
+            std::cout << "   1 2 3\n";
             for (int i = 0; i < 3; i++) {
-                std::cout << i+ 1 << " |";
+                std::cout << i + 1 << " |";
                 for (int j = 0; j < 3; j++) {
                     std::cout << field[i][j] << "|";
                 }
                 std::cout << "\n";
             }
-            std::cout << "Choose the column and the row:" << std::endl;
-            std::cin >> playerCol >> playerRow;
-            field[playerCol + 1][playerRow + 1] = player;
+            std::cout << "Player " << player << ": choose the row and the column:" << std::endl;
+            std::cin >> playerRow >> playerCol;
+            field[playerRow - 1][playerCol - 1] = player;
+
+            if (player == 'x') {
+                player = 'o';
+            } else {
+                player = 'x';
+            }
         }
     }
 }
